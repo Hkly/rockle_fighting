056b2c41e4c588ac96b1a7935a75b5cb6acabd0d
Hannah Yiu <hannahkyiu@gmail.com>
2014-09-10 16:26:14 -0700

    create Users table

---
+++ test/models/user_test.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string(255)      not null
+#  password_digest :string(255)      not null
+#  session_token   :string(255)      not null
+#  gold            :integer          default(100)
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
