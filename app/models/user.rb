033c8f13c4122431a68c0c5d8166d3f6bc43a88a
Hannah Yiu <hannahkyiu@gmail.com>
2014-09-10 16:42:28 -0700

    basic user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,46 @@
 #
 
 class User < ActiveRecord::Base
+  attr_reader :password
+
+  before_validation :ensure_session_token
+
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true}
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    if user.nil? || !user.is_password?(password)
+      return nil
+    end
+    return user
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    unless password.nil?
+      @password = password
+      self.password_digest = BCrypt::Password.create(password)
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
